fa6e1a973f12dc2f93a2b19fb406a87d97c916b3
Mark W. Dennehy <mhlrsym9@comcast.net>
2019-03-18 20:29:23 -0400

    Add turns, attackers, and phases.

--- src/asl_recorder/core.clj
+++ src/asl_recorder/core.clj
@@ -45,6 +45,33 @@
   (sc/text! attacker next-attacker)
   (sc/text! phase next-phase))
 
+(defn append-event [loc the-action the-result]
+  (let [n (zip/node loc)
+        c (conj (:content n) (xml/element :event {} (list (xml/element :action {} the-action)
+                                                          (xml/element :result {} the-result))))]
+    (zip/replace loc (assoc n :content c))))
+
+(defn append-phase [loc the-phase]
+  (let [l (zip/up loc)
+        n (zip/node l)
+        c (conj (:content n) (xml/element :phase {:name the-phase}))]
+    (zip/replace l (assoc n :content c))))
+
+(defn append-attacker [loc the-attacker]
+  (let [l (-> loc zip/up zip/up)
+        n (zip/node l)
+        c (conj (:content n) (xml/element :side {:attacker the-attacker}
+                                          (xml/element :phase {:name "Rally"})))]
+    (zip/replace l (assoc n :content c))))
+
+(defn append-turn [loc the-turn]
+  (let [l (-> loc zip/up zip/up zip/up)
+        n (zip/node l)
+        c (conj (:content n) (xml/element :turn {:number the-turn}
+                                          (xml/element :side {:attacker "German"}
+                                                       (xml/element :phase {:name "Rally"}))))]
+    (zip/replace l (assoc n :content c))))
+
 (defn- advance-turn [turn attacker phase]
   (fn [_] (let [next-turn (inc-turn turn)]
             (update-time turn next-turn attacker "German" phase "Rally"))))
@@ -59,24 +86,24 @@
 (defn- advance-phase [turn attacker phase]
   (fn [_] (let [next-phase (get phase-map (sc/text phase))
                 current-attacker (sc/text attacker)
-                rally-phase? (= next-phase "Rally")
-                next-attacker (if rally-phase?
+                next-attacker? (= next-phase "Rally")
+                next-attacker (if next-attacker?
                                 (get attacker-map current-attacker)
                                 current-attacker)
-                next-turn (if (and rally-phase? (= next-attacker "German"))
+                next-turn? (and next-attacker? (= next-attacker "German"))
+                next-turn (if next-turn?
                             (inc-turn turn)
                             (sc/text turn))]
+            (cond next-turn? (swap! game-zip-loc append-turn next-turn)
+                  next-attacker? (swap! game-zip-loc append-attacker next-attacker)
+                  :else (swap! game-zip-loc append-phase next-phase))
             (update-time turn next-turn attacker next-attacker phase next-phase))))
 
-(defn append-event [loc the-event]
-  (let [n (zip/node loc)
-        c (conj (:content n) (xml/element :event {} the-event))]
-    (zip/replace loc (assoc n :content c))))
-
-(defn- add-event [event]
+(defn- add-event [action result]
   (fn [_]
-    (swap! game-zip-loc append-event (sc/text event))
-    (sc/text! event "")))
+    (swap! game-zip-loc append-event (sc/text action) (sc/text result))
+    (sc/text! action "")
+    (sc/text! result "")))
 
 (defn -main
   [& args]
@@ -87,19 +114,22 @@
                       (sc/button :id :advance-attacker-button :text "Next attacker")
                       (sc/label :id :phase :text "Rally")
                       (sc/button :id :advance-phase-button :text "Next phase")
-                      (sc/text :id :event)
+                      (sc/text :id :action)
+                      (sc/text :id :result)
                       (sc/button :id :add-event-button :text "Add event")
                       (sc/button :id :ok :text "OK" :enabled? false)]
                      (let [ok-fn (fn [e] (process-the-game e))]
                        (sc/listen advance-turn-button :action (advance-turn turn attacker phase))
                        (sc/listen advance-attacker-button :action (advance-attacker turn attacker phase))
                        (sc/listen advance-phase-button :action (advance-phase turn attacker phase))
-                       (sc/listen add-event-button :action (add-event event))
+                       (sc/listen add-event-button :action (add-event action result))
                        (sc/listen ok :action ok-fn)
                        (-> (sc/frame :title "ASL Recorder",
                                      :content (sm/mig-panel :constraints [] :items [["Turn:"] [turn "w 100!"] ["Attacker:"] [attacker "w 100!"] ["Phase:"] [phase "w 100!, wrap"]
                                                                                     [advance-turn-button "span 2, align center"] [advance-attacker-button "span 2, align center"] [advance-phase-button "span 2, align center, wrap"]
-                                                                                    ["Event:"] [event "span 4, growx"] [add-event-button "wrap"]
+                                                                                    ["Action:"] [action "span 5, growx, wrap"]
+                                                                                    ["Result:"] [result "span 5, growx, wrap"]
+                                                                                    [add-event-button "span, align center, wrap"]
                                                                                     [ok "span, align center"]]),
                                      :on-close :exit)
                            sc/pack!
--- test/asl_recorder/core_test.clj
+++ test/asl_recorder/core_test.clj
@@ -3,8 +3,32 @@
             [asl-recorder.core :refer :all]
             [clojure.zip :as zip]))
 
-(deftest a-test
+(deftest test-append-event
   (testing "Try to add an event."
-    (let [new-loc (append-event @game-zip-loc "Add event")
-          fc (-> new-loc zip/root zip/xml-zip zip/down zip/down zip/down zip/node :content first :content first)]
-      (is (= fc "Add event")))))
+    (let [new-loc (append-event @game-zip-loc "Add action" "Add result")
+          fe (-> new-loc zip/root zip/xml-zip zip/down zip/down zip/down zip/node :content first :content first)
+          fa (-> fe first :content first)
+          fr (-> fe second :content first)]
+      (is (= fa "Add action"))
+      (is (= fr "Add result")))))
+
+(deftest test-append-phase
+  (testing "Try to add a phase."
+    (let [new-loc (append-phase @game-zip-loc "Prep Fire")
+          fp (-> new-loc zip/root zip/xml-zip zip/down zip/down zip/node :content)]
+      (is (= "Prep Fire" (-> fp first :attrs :name)))
+      (is (= "Rally" (-> fp second :attrs :name))))))
+
+(deftest test-append-attacker
+  (testing "Try to add an attacker."
+    (let [new-loc (append-attacker @game-zip-loc "Russian")
+          fp (-> new-loc zip/root zip/xml-zip zip/down zip/node :content)]
+      (is (= "Russian" (-> fp first :attrs :attacker)))
+      (is (= "German" (-> fp second :attrs :attacker))))))
+
+(deftest test-append-turn
+  (testing "Try to add an turn."
+    (let [new-loc (append-turn @game-zip-loc 2)
+          fp (-> new-loc zip/root zip/xml-zip zip/node :content)]
+      (is (= 2 (-> fp first :attrs :number)))
+      (is (= 1 (-> fp second :attrs :number))))))
